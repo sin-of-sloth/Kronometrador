2c3f5fb63cd5b6d13896e995e8d5b4cdafaf7ecd
sin-of-sloth <arjunlal0709@gmail.com>
2020-12-21 17:10:30 +0530

    authentication stuff

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Authentication
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+
+    #Todos
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('', views.home, name='home'),
 ]
